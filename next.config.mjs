525a830fbb9fbf6a598252df49966319dc4b2d82
Renaud Redron <renaud.redron@gmail.com>
2024-08-26 16:03:29 +0200

    Ajout du hero

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['flowbite.s3.amazonaws.com'], // Add the domain here
+    },
+};
 
 export default nextConfig;
